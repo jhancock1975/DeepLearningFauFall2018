ad3001eddcffc79525d252e50285fc94d058e770
John Hancock <jhancock1975@gmail.com>
2018-09-15 17:01:43 -0400

    parese arguments

--- homework-1/pdf-encrpyt-commit.sh
+++ homework-1/pdf-encrpyt-commit.sh
@@ -1,8 +1,40 @@
 #!/bin/bash
-file_prefix=$1
-branch=$2
+function usage(){
+echo "usage: $0 -f tex_file_name_prefix -b branch_name"
+}  
+
+while getopts ":f:b:" opt; do
+  case $opt in
+    f)
+     file_prefix=$OPTARG 
+     echo file_prefix = $file_prefix
+     ;;
+    b)
+     branch=$OPTARG
+     echo branch = $branch
+     ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      usage
+      exit 1
+      ;;
+  esac
+done
+
+if [ -z "$file_prefix" ]
+then
+   usage
+   exit 1
+fi
+
+if [ -z "$branch" ]
+then
+  usage
+  exit 1
+fi
+
 pdflatex ${file_prefix}.tex
-gpg -r jhancock1975@gmail.com -y -e ${file_prefix}.tex
-git add ${file_prefix}.tex
+gpg -r jhancock1975@gmail.com --yes -e ${file_prefix}.tex
+git add ${file_prefix}.tex.gpg
 git commit -m "working on ${file_prefix}"
 git push -u origin ${branch}
